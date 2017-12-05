50e28d74b9ee4da488d55cde60a68eb02f2d4fda
woyaodeyangguang <603819372@qq.com>
2017-12-05 15:26:12 +0800

    initial project

---
+++ src/main/java/com/test/Singletion.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class Singletion
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
