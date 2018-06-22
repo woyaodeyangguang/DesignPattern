132e5eb87153cb780582cff82b267f6ab1ec9cc1
woyaodeyangguang <603819372@qq.com>
2018-06-22 09:41:40 +0800

    update static inner singleton.

--- src/main/java/com/test/singletion/StaticInnerSingleton.java
+++ src/main/java/com/test/singletion/StaticInnerSingleton.java
@@ -7,9 +7,12 @@ public class StaticInnerSingleton {
 
   private static class LazyHolder {
     private static final StaticInnerSingleton instance = new StaticInnerSingleton();
-
-    private LazyHolder() {}
+    static {
+      System.out.println("LazyHolder init");
+    }
   }
+
+  private StaticInnerSingleton() {}
 
   /**
    * 只有真正调用getInstance方法时,才会加载静态内部类(延迟加载)
@@ -18,4 +21,5 @@ public class StaticInnerSingleton {
   public static StaticInnerSingleton getInstance() {
     return LazyHolder.instance;
   }
+
 }
